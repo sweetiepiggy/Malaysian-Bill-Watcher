67537d8f238f77bb4037bf0f6d8ccda24139ce05
Sweetie Piggy <sweetiepiggyapps@gmail.com>
2012-05-09 21:08:35 +0800

    add options menu to display gpl

--- android/src/org/sinarproject/malaysianbillwatcher/MalaysianBillWatcherActivity.java
+++ android/src/org/sinarproject/malaysianbillwatcher/MalaysianBillWatcherActivity.java
@@ -22,6 +22,9 @@ package org.sinarproject.malaysianbillwatcher;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -62,5 +65,26 @@ public class MalaysianBillWatcherActivity extends Activity {
 			}
 		});
 	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.options_menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.about:
+			Intent intent = new Intent(getApplicationContext(), AboutActivity.class);
+			startActivity(intent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+
+
 }
 
